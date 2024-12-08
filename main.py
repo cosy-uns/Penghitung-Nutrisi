7f6ddbd5799593b5ae70c848e47cbb93d138772a
yamillakirana <yamillakirana@student.uns.ac.id>
2024-12-08 22:10:22 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from app_nutri import AplikasiNutrisi
+
+def main():
+    root = tk.Tk()
+    AplikasiNutrisi(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
